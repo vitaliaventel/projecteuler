23058f454ceff6fec22a07b370b563801759ae93
Vitalii Leshchenko <vitaliaventel@gmail.com>
2017-07-27 12:51:09 +0300

    Runner class added

---
+++ src/Runner.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Vitalii Leshchenko on 27.07.2017.
+ */
+
+public class Runner {
+
+    public static void main(String[] args) {
+        //MultiplesOf3And5
+        System.out.println("Task 1, result = " + MultiplesOf3And5.sum(1000));
+    }
+
+}
