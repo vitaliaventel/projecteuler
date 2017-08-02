46db67c156179587f1e5c48f72d5cabfde676c2b
Vitalii Leshchenko <vitaliaventel@gmail.com>
2017-08-02 17:59:36 +0300

    Runner class update, task 17 added

--- src/Runner.java
+++ src/Runner.java
@@ -50,6 +50,9 @@ public class Runner {
         //Largest product in a series
         System.out.println("Task 16, result = " + PowerDigit.sum(2, 1000));
 
+        //Largest product in a series
+        System.out.println("Task 17, result = " + NumberLetter.letterCount(1000));
+        System.out.println(126 % 100 % 10 );
     }
 
 }
