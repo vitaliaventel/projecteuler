fb2c4675bc88f1437e612647338ade8ba4c4fc41
Vitalii Leshchenko <vitaliaventel@gmail.com>
2017-08-09 17:53:28 +0300

    Task 19: DayCounter class added

---
+++ src/DayCounter.java
@@ -0,0 +1,38 @@
+/**
+ * Created by Vitalii Leshchenko on 09.08.2017.
+ */
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * You are given the following information, but you may prefer to do some research for yourself.
+ * 1 Jan 1900 was a Monday.
+ * Thirty days has September,
+ * April, June and November.
+ * All the rest have thirty-one,
+ * Saving February alone,
+ * Which has twenty-eight, rain or shine.
+ * And on leap years, twenty-nine.
+ * A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
+ * How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+ */
+
+public class DayCounter {
+
+    public static int sundaysBetweenDates(Date start, Date finish) {
+        int sundays = 0;
+        Calendar cal = Calendar.getInstance();
+        while (sundays != 171) {
+            cal.setTime(start);
+            if ((cal.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY) && (cal.get(Calendar.DAY_OF_MONTH) == 1)) {
+                System.out.println(cal.toString());
+                sundays++;
+            }
+            cal.add(Calendar.DATE, 1);
+            start = cal.getTime();
+        }
+        System.out.println(cal.toString());
+        return sundays;
+    }
+}
